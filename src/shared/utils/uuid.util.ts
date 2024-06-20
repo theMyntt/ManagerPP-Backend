8ec05770b323fce7f934ddca871e38963c91ab33
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-19 22:12:22 -0300

    feat: generate uuid entity

---
+++ src/shared/utils/uuid.util.ts
@@ -0,0 +1,27 @@
+export class UUID {
+  /**
+   * Generates a random UUID.
+   * @returns A string representation of the generated UUID.
+   */
+  static generate() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c == 'x' ? r : (r & 0x3) | 0x8
+        return v.toString(16)
+      }
+    )
+  }
+
+  /**
+   * Validates a given UUID string.
+   * @param uuid - The UUID string to validate.
+   * @returns True if the UUID is valid, false otherwise.
+   */
+  static validate(uuid: string) {
+    return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+      uuid
+    )
+  }
+}
