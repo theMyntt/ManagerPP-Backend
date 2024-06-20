08d3b03fd9987eea8c2183e0b932940e41537acd
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-19 22:01:26 -0300

    feat: log nest port, and mysql informations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { Logger } from '@nestjs/common'
+import { env } from './config/env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  await app.listen(env.NEST_PORT)
+
+  Logger.log(`Application running at localhost:${env.NEST_PORT}`)
+  Logger.log(
+    `================================= ${env.NEST_PRODUCTION ? 'PRODUCTION' : 'DEVELOPMENT'} =================================`
+  )
+  Logger.log(`MySQL connected at: ${env.MYSQL_HOST}:${env.MYSQL_PORT}`)
+  Logger.log(`MySQL database: ${env.MYSQL_DATABASE}`)
+  Logger.log(`MySQL connected user: ${env.MYSQL_USERNAME}`)
 }
 bootstrap()
