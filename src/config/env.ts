632004fd9024045423839e424a0e57fea484d05b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-19 22:40:45 -0300

    fix: env usage

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,18 +1,10 @@
-const ensureEnvVariable = (key: string): string => {
-  const value = process.env[key]
-  if (!value) {
-    throw new Error(`${key} is not provided`)
-  }
-  return value
-}
-
-export const env = {
-  NEST_PORT: parseInt(process.env.NEST_PORT, 10) || 1023,
+export default () => ({
+  NEST_PORT: parseInt(process.env.NEST_PORT, 10) || 3000,
   NEST_PRODUCTION: process.env.NEST_PRODUCTION === 'true',
   MYSQL_PORT: parseInt(process.env.MYSQL_PORT, 10) || 3306,
   MYSQL_HOST: process.env.MYSQL_HOST || 'localhost',
   MYSQL_USERNAME: process.env.MYSQL_USERNAME || 'root',
   MYSQL_PASSWORD: process.env.MYSQL_PASSWORD || '',
-  MYSQL_DATABASE: ensureEnvVariable('MYSQL_DATABASE'),
+  MYSQL_DATABASE: process.env.MYSQL_DATABASE || 'manager_db',
   MYSQL_SYNCRONIZE: process.env.MYSQL_SYNCRONIZE === 'true'
-}
+})
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { Logger } from '@nestjs/common'
-import { env } from './config/env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  await app.listen(env.NEST_PORT)
+  await app.listen(process.env.NEST_PORT)
 
-  Logger.log(`Application running at localhost:${env.NEST_PORT}`)
+  Logger.log(`Application running at localhost:${process.env.NEST_PORT}`)
   Logger.log(
-    `================================= ${env.NEST_PRODUCTION ? 'PRODUCTION' : 'DEVELOPMENT'} =================================`
+    `================================= ${process.env.NEST_PRODUCTION ? 'PRODUCTION' : 'DEVELOPMENT'} =================================`
   )
-  Logger.log(`MySQL connected at: ${env.MYSQL_HOST}:${env.MYSQL_PORT}`)
-  Logger.log(`MySQL database: ${env.MYSQL_DATABASE}`)
-  Logger.log(`MySQL connected user: ${env.MYSQL_USERNAME}`)
+  Logger.log(
+    `MySQL connected at: ${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT}`
+  )
+  Logger.log(`MySQL database: ${process.env.MYSQL_DATABASE}`)
+  Logger.log(`MySQL connected user: ${process.env.MYSQL_USERNAME}`)
 }
 bootstrap()
