00fff81b3f0a5d56990f76b2ebd5ee7bfea4e28b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-20 22:58:07 -0300

    fix: types errors

--- src/user/domain/repositories/user.repository.ts
+++ src/user/domain/repositories/user.repository.ts
@@ -1,8 +1,10 @@
 import { IRepositoryContract } from '@shared/contracts/repository.contract'
+import { UserEntity } from '@src/user/infra/database/orm/user.entity'
 
-export interface IUserRepository<I, O> extends IRepositoryContract<I, O> {
+export interface IUserRepository<I, O>
+  extends IRepositoryContract<I, O | UserEntity | Array<UserEntity>> {
   create(dto: I): Promise<O>
-  find(dto: I): Promise<O>
+  find(dto: I): Promise<UserEntity>
   update(dto: I): Promise<O>
   delete(dto: I): Promise<O>
 }
--- src/user/infra/repositories/user.repository.ts
+++ src/user/infra/repositories/user.repository.ts
@@ -4,13 +4,17 @@ import { UserEntity } from '../database/orm/user.entity'
 import { Repository } from 'typeorm'
 import { IResult } from '@shared/core/result.core'
 
-export class UserRepository<I, O>
+export class UserRepository<I>
   implements IUserRepository<I, IResult | UserEntity>
 {
   constructor(
     @InjectRepository(UserEntity)
     private readonly schema: Repository<UserEntity>
   ) {}
+
+  findAll(dto: I, limit: any): Promise<UserEntity[] | IResult> {
+    throw new Error('Method not implemented.')
+  }
 
   async create(dto: I): Promise<IResult> {
     await this.schema.save(dto)
@@ -27,9 +31,6 @@ export class UserRepository<I, O>
     throw new Error('Method not implemented.')
   }
   delete(dto: I): Promise<IResult> {
-    throw new Error('Method not implemented.')
-  }
-  findAll(dto: I, limit: any): Promise<UserEntity[]> {
     throw new Error('Method not implemented.')
   }
 }
