4ff8fda7f9a0bfe3cc55c646bfe531be578cfb14
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-21 16:26:35 -0300

    test: error entities spec

---
+++ test/units/user/app/errors/create.error.spec.ts
@@ -0,0 +1,31 @@
+import {
+  InvalidInformations,
+  AlreadyExists
+} from '@src/user/app/errors/create.error'
+import { IResult } from '@shared/core/result.core'
+
+describe('Error Classes', () => {
+  describe('AlreadyExists', () => {
+    it('should return the correct error result', () => {
+      const errorInstance = new AlreadyExists()
+      const result: IResult = errorInstance.new()
+
+      expect(result).toEqual({
+        message: 'User already exists',
+        statusCode: 400
+      })
+    })
+  })
+
+  describe('InvalidInformations', () => {
+    it('should return the correct error result', () => {
+      const errorInstance = new InvalidInformations()
+      const result: IResult = errorInstance.new()
+
+      expect(result).toEqual({
+        message: 'Invalid informations',
+        statusCode: 400
+      })
+    })
+  })
+})
---
+++ test/units/user/app/errors/login.error.spec.ts
@@ -0,0 +1,14 @@
+import { IResult } from '@shared/core/result.core'
+import { NotFound } from '@src/user/app/errors/login.error'
+
+describe('NotFound', () => {
+  it('should return the correct error result', () => {
+    const errorInstance = new NotFound()
+    const result: IResult = errorInstance.new()
+
+    expect(result).toEqual({
+      message: 'No user found',
+      statusCode: 404
+    })
+  })
+})
