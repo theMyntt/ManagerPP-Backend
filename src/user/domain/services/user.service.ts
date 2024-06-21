b1ed2d8d56aea6206071b9fcd72561ac37c4e086
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-20 23:22:51 -0300

    feat: find service

--- src/user/domain/services/user.service.ts
+++ src/user/domain/services/user.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common'
 import { UserRepository } from '@src/user/infra/repositories/user.repository'
 import { IResult } from '@shared/core/result.core'
 import { UserEntity } from '@src/user/infra/database/orm/user.entity'
+import { LoginUserDTO } from '@src/user/app/dto/login.dto'
 
 @Injectable()
 export class UserService {
@@ -23,5 +24,9 @@ export class UserService {
       message: 'User created',
       statusCode: 200
     }
+  }
+
+  async find(dto: LoginUserDTO): Promise<UserEntity> {
+    return await this.repo.find(dto)
   }
 }
