905345cf97079143ae4072603a4a6f7696393da8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-20 23:23:18 -0300

    chore: implementation of useCase and controller

--- src/user/infra/user.module.ts
+++ src/user/infra/user.module.ts
@@ -5,14 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { CreateUseCase } from '../app/usecases/create.usecase'
 import { UserService } from '../domain/services/user.service'
 import { CreateController } from './controllers/create.controller'
+import { LoginUseCase } from '../app/usecases/login.usecase'
+import { LoginController } from './controllers/login.controller'
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  controllers: [CreateController],
+  controllers: [CreateController, LoginController],
   providers: [
     { provide: 'I_USER_REPOSITORY', useClass: UserRepository },
     { provide: 'I_USER_ENTITY', useClass: UserEntity },
     { provide: 'I_CREATE_USECASE', useClass: CreateUseCase },
+    { provide: 'I_LOGIN_USECASE', useClass: LoginUseCase },
     UserService
   ]
 })
