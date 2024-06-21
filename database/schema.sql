d55dd4650ec7dbcec551e1a3d210861fdfb7ef69
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-21 18:41:03 -0300

    feat: test db sql schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,8 @@
--- MySQL Code
-
+-- DEFAULT DB
 drop database if exists manager_db;
 create database manager_db;
 use manager_db;
+
 
 create table users(
 	id varchar(255) not null,
@@ -18,4 +18,20 @@ create table users(
 
 insert into users values
 	('311e6eda-266e-4423-98e2-236b01fd6fee', 'ADMIN1', 'ADMIN', 'admin@admin.org', '12345678', curdate(), curdate());
-    
+
+-- TEST DB
+drop database if exists manager_db_test;
+create database manager_db_test;
+use manager_db_test;
+
+create table users(
+	id varchar(255) not null,
+    access_code varchar(7) not null,
+    name varchar(80) not null,
+    email varchar(90) not null,
+    password varchar(255) not null,
+    createdAt date not null,
+    updatedAt date not null,
+    primary key (id),
+    unique(access_code)
+);
