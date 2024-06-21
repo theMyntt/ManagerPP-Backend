71e1c5111385e6a89122ca36d0a0ce8ae3e769d3
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-20 22:23:38 -0300

    feat: create user service

---
+++ src/user/domain/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { UserRepository } from '@src/user/infra/repositories/user.repository'
+import { IResult } from '@shared/core/result.core'
+import { UserEntity } from '@src/user/infra/database/orm/user.entity'
+
+@Injectable()
+export class UserService {
+  constructor(
+    @Inject('I_USER_REPOSITORY')
+    private readonly repo: UserRepository<any, IResult>
+  ) {}
+
+  async create(dto: UserEntity): Promise<IResult> {
+    if (await this.repo.find(dto)) return
+
+    await this.repo.create(dto)
+
+    return {
+      message: 'User created',
+      statusCode: 200
+    }
+  }
+}
