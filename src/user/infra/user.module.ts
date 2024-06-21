dea22059da20ac36109389c1ef9ffc2067b4a551
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-20 22:58:25 -0300

    chore: provindg useCase and userService

--- src/user/infra/user.module.ts
+++ src/user/infra/user.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common'
 import { UserRepository } from './repositories/user.repository'
 import { UserEntity } from './database/orm/user.entity'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { CreateUseCase } from '../app/usecases/create.usecase'
+import { UserService } from '../domain/services/user.service'
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [],
   providers: [
     { provide: 'I_USER_REPOSITORY', useClass: UserRepository },
-    { provide: 'I_USER_ENTITY', useClass: UserEntity }
+    { provide: 'I_USER_ENTITY', useClass: UserEntity },
+    { provide: 'I_CREATE_USECASE', useClass: CreateUseCase },
+    UserService
   ]
 })
 export class UserModule {}
