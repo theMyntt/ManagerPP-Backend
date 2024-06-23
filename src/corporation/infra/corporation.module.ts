e3d1fb506e0f921951955846b1b361ec9fd5fa11
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-23 16:00:06 -0300

    chore: provading CorporationService at corporation context

--- src/corporation/infra/corporation.module.ts
+++ src/corporation/infra/corporation.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { CorporationEntity } from './entities/corporation.entity'
 import { CorporationRepository } from './repositories/corporation.repository'
+import { CorporationService } from '../domain/services/corporation.service'
 
 @Module({
   imports: [TypeOrmModule.forFeature([CorporationEntity])],
   providers: [
     { provide: 'I_CORPORATION_REPOSITORY', useClass: CorporationRepository },
-    { provide: 'I_CORPORATION_ENTITY', useClass: CorporationEntity }
+    { provide: 'I_CORPORATION_ENTITY', useClass: CorporationEntity },
+    CorporationService
   ]
 })
 export class CorporationModule {}
