9d475daa3a66fadc72a4f1e9118a2a970a3e7d7a
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-23 15:53:21 -0300

    chore: providing repository and entity at corporation context

--- src/corporation/infra/corporation.module.ts
+++ src/corporation/infra/corporation.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { CorporationEntity } from './entities/corporation.entity'
+import { CorporationRepository } from './repositories/corporation.repository'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CorporationEntity])]
+  imports: [TypeOrmModule.forFeature([CorporationEntity])],
+  providers: [
+    { provide: 'I_CORPORATION_REPOSITORY', useClass: CorporationRepository },
+    { provide: 'I_CORPORATION_ENTITY', useClass: CorporationEntity }
+  ]
 })
 export class CorporationModule {}
