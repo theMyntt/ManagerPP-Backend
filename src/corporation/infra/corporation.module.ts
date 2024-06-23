f6c4f7bdf67909a94156ca9843c46b47ff55f556
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-23 16:09:03 -0300

    fix: not providing controller at corporation context

--- src/corporation/infra/corporation.module.ts
+++ src/corporation/infra/corporation.module.ts
@@ -4,9 +4,11 @@ import { CorporationEntity } from './entities/corporation.entity'
 import { CorporationRepository } from './repositories/corporation.repository'
 import { CorporationService } from '../domain/services/corporation.service'
 import { CreateCorporationUseCase } from '../app/usecases/create.usecase'
+import { CreateCorporationController } from './controllers/create.controller'
 
 @Module({
   imports: [TypeOrmModule.forFeature([CorporationEntity])],
+  controllers: [CreateCorporationController],
   providers: [
     { provide: 'I_CORPORATION_REPOSITORY', useClass: CorporationRepository },
     { provide: 'I_CORPORATION_ENTITY', useClass: CorporationEntity },
