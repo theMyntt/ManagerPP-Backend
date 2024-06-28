d8d52821a873b4f3cde40affd40157e9a8142edc
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-28 15:53:55 -0300

    chore: implementation of auth middleware

--- src/corporation/infra/controllers/delete.controller.ts
+++ src/corporation/infra/controllers/delete.controller.ts
@@ -1,10 +1,14 @@
 import { IControllerContract } from '@shared/domain/contracts/controller.contract'
 import { DeleteCorporationDTO } from '../dto/delete.dto'
 import { IResult } from '@shared/domain/core/result.core'
-import { Controller, Delete, Inject, Query } from '@nestjs/common'
+import { Controller, Delete, Inject, Query, UseGuards } from '@nestjs/common'
 import { DeleteCorporationUseCase } from '@src/corporation/app/usecases/delete.usecase'
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { AuthMiddleware } from '@shared/infra/middlewares/auth.middleware'
 
 @Controller('corporation')
+@UseGuards(AuthMiddleware)
+@ApiTags('Corporation Management')
 export class DeleteCorporationController
   implements IControllerContract<string, IResult>
 {
@@ -14,6 +18,9 @@ export class DeleteCorporationController
   ) {}
 
   @Delete('v1/delete')
+  @ApiBearerAuth('authorization')
+  @ApiResponse({ status: 201, description: 'Corporation successfuly deleted' })
+  @ApiResponse({ status: 404, description: 'Corporation not found' })
   async perform(@Query('email') email: string): Promise<IResult> {
     const dto = new DeleteCorporationDTO()
     dto.email = email
